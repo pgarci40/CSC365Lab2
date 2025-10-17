33e2aeddd043183a7e9ed4941d56f6a2c3d68287
pgarci40 <148913223+pgarci40@users.noreply.github.com>
2025-10-16 21:20:04 -0700

    Update STUDENTS.sql

--- STUDENTS.sql
+++ STUDENTS.sql
@@ -1,17 +1,1 @@
-DROP TABLE IF EXISTS List;
-DROP TABLE IF EXISTS Teachers;
 
-CREATE TABLE TEACHERS(
-    LastName VARCHAR(50) NOT NULL,
-    FirstName VARCHAR(50) NOT NULL,
-    Classroom INT PRIMARY KEY
-);
-
-CREATE TABLE LIST(
-    LastName VARCHAR(50) NOT NULL,
-    FirstName VARCHAR(50) NOT NULL,
-    Grade INT,
-    Classroom INT NOT NULL,
-    PRIMARY KEY (LastName, FirstName, Classroom),
-    FOREIGN KEY (Classroom) REFERENCES TEACHERS(Classroom)
-);
